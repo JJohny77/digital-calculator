1124892de50d30e2154db119277380abe44daf36
KostasMartzoukos <martzkostas@gmail.com>
2025-11-07 17:27:45 +0200

    enha:προσθηκη sin στο cos (#18)

---
+++ src/CosSin.java
@@ -0,0 +1,12 @@
+public class CosSin {
+    public static double cosine(double x)
+    {
+        return Math.cos(Math.toRadians(x));
+    }
+
+
+    public static double sine(double x) {
+            return Math.sin(Math.toRadians(x));
+    }
+
+}
--- src/Cosine.java
+++
@@ -1,5 +0,0 @@
-public class Cosine {
-    public static double cosine(double x) {
-        return Math.cos(Math.toRadians(x));
-    }
-}
